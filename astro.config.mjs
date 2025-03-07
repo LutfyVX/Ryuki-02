34612a4f4fdafd24b47ec250dd1796d049030a05
KazyaVX <kazuyaicikawa@gmail.com>
2025-03-07 23:10:07 +0700

    The..

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react"; 
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    prefetch(),
     react(), 
   ],
   vite: {
